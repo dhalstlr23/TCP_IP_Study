8fbb34e5593698efe18f1a332ceda689cc6284d2
dhalstlr23 <oms9302@gmail.com>
2025-09-05 09:48:01 +0900

    First commit of TCP-IP project

---
+++ Client/Echo_client.cpp
@@ -0,0 +1,78 @@
+﻿#pragma comment(lib, "ws2_32") // 반드시 맨위에 코딩해야함.
+#include <WinSock2.h>
+#include <iostream>
+//#include <cstdio>
+#include <string>
+#include <array>
+#include <WS2tcpip.h>
+
+int main(int argc, const char* argv[])
+{
+    std::cout << "윈도우 기반 TCP-IP \n" << std::endl;
+    WSADATA wsadata; // wsadata 객체 생성
+    if (WSAStartup(MAKEWORD(2, 2), &wsadata) != 0)
+    {
+        std::cerr << "socket error" << std::endl;
+        return EXIT_FAILURE;
+    }
+    if (argc != 3)
+    {
+        std::cerr << "사용방법 " << argv[0] << "IP_NUMBER " << "PORT" << std::endl;
+        return EXIT_FAILURE;
+
+    }
+
+    SOCKET server_socket;
+    SOCKADDR_IN server_addr;    // 서버의 구조를 담는 구조체
+    server_socket = socket(PF_INET /*IPv4*/, SOCK_STREAM/*TCP*/, IPPROTO_TCP);
+    if (server_socket == INVALID_SOCKET)
+    {
+        std::cout << "socket() error" << std::endl;
+        return EXIT_FAILURE;
+    }
+    memset(&server_addr, 0, sizeof server_addr); // 초기화
+    server_addr.sin_family = AF_INET; // IPv4 PF_INET
+    //server_addr.sin_addr.s_addr = inet_addr(argv[1]); // "192.168.100.200"  Deprecated
+    if (inet_pton(AF_INET, argv[1], &(server_addr.sin_addr)) == 0)
+    {
+        std::cout << "inet_pton() error" << std::endl;
+        return EXIT_FAILURE;
+    }
+    server_addr.sin_port = htons(std::stoi(argv[2])); // "9999" -> 9999
+    if (connect(server_socket, (const SOCKADDR*)&server_addr, sizeof server_addr) == SOCKET_ERROR)
+    {
+        std::cout << "connect() error" << std::endl;
+        return EXIT_FAILURE;
+    }
+    char buffer[BUFSIZ]{ '\0' };
+    int str_length = 0;
+    while (true)
+    {
+        std::string message; // string
+        std::cout << "전송할 문자를 입력해주세요  ";
+        std::getline(std::cin, message);
+        send(server_socket, (const char*)message.c_str(), message.size(), 0);
+        if ((str_length = recv(server_socket, buffer, sizeof buffer, 0)) > 0)
+        {
+            std::cout << "서버에서 보낸 문자 :   " << buffer << std::endl;
+        }
+    }
+
+
+
+    /*int read_length = recv(server_socket, reinterpret_cast<char*>(buffer), sizeof buffer, 0);
+    if (read_length == -1)
+    {
+        std::cout << "recv() error" << std::endl;
+    }*/
+    //std::cout << buffer << std::endl;
+    closesocket(server_socket);
+
+
+
+    // 맨 마지막에 넣어 줘야 함
+    WSACleanup(); // 원속 종료
+    return 0;
+}
+
+
---
+++ Server/Echo_Server.cpp
@@ -0,0 +1,83 @@
+﻿// Echo_Server.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#pragma comment(lib, "ws2_32") // 반드시 맨위에 코딩해야함.
+#include <WinSock2.h>
+#include <iostream>
+//#include <cstdio>
+#include <string>
+#include <array>
+
+int main(int argc, const char* argv[])
+{
+    std::cout << "윈도우 기반 TCP-IP \n" << std::endl;
+    WSADATA wsadata; // wsadata 객체 생성
+    if (WSAStartup(MAKEWORD(2, 2), &wsadata) != 0)
+    {
+        std::cerr << "socket error" << std::endl;
+        return EXIT_FAILURE;
+    }
+    if (argc != 2) {
+        std::cerr << "사용방법은 " << argv[0] << " 9999" << std::endl;
+        return EXIT_FAILURE;
+    }
+    SOCKET server_sock{ 0ull }; // uniform initializer
+    SOCKET client_sock{ 0ull };
+    SOCKADDR_IN server_addr; // server 주소랑 IP정보등을 담는 구조체
+    SOCKADDR_IN client_addr; // client 주소랑 IP정보등을 담는 구조체
+    memset(&server_addr, 0, sizeof server_addr);
+    memset(&client_addr, 0, sizeof client_addr);
+    // std::array<char, BUFSIZ> buffer{ "Welcome My server!!" };
+
+    /*
+    * 서버의 라이프 사이클(함수) 만들기
+    */
+    server_sock = socket(PF_INET, SOCK_STREAM, 0); // TCP연결
+    if (server_sock == INVALID_SOCKET)
+    {
+        std::cout << "socket() error" << std::endl;
+        return EXIT_FAILURE;
+    }
+    server_addr.sin_family = AF_INET; // PF_INET : IPv4
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY); // 내 컴퓨터의 IP : 10.10.200.70
+    server_addr.sin_port = htons(std::stoi(argv[1]));  // 문자열 -> 정수값 PORT
+    size_t client_size{ 0ull }; // 클라이언트의 구조체 사이즈 크기를 저장하는 변수
+    if (bind(server_sock, reinterpret_cast<const struct sockaddr*>(&server_addr), sizeof server_addr) == SOCKET_ERROR)
+    {
+        std::cout << "bind() error" << std::endl;
+        return EXIT_FAILURE;
+    }
+    if (listen(server_sock, 5) == SOCKET_ERROR)
+    {
+        std::cout << "listen() error" << std::endl;
+        return EXIT_FAILURE;
+    }
+    client_size = sizeof client_addr; // 클라이언트 구조체의 사이즈
+    client_sock = accept(server_sock, reinterpret_cast<struct sockaddr*>(&client_addr), reinterpret_cast<int*>(&client_size));
+    if (client_sock == INVALID_SOCKET)
+
+
+    {
+        std::cout << "accept() error" << std::endl;
+        return EXIT_FAILURE;
+    }
+    // send(client_sock, buffer.data(), buffer.size(), 0);// 클라이언트에게 문자열을 보내기
+    int str_length = 0;
+    char buffer[BUFSIZ]{ '\O' };
+    while ((str_length = recv(client_sock, buffer, sizeof buffer, 0)) > 0)
+    {
+        /*서버 모드*/
+        std::cout << "클라이언트가 보낸 문자 : " << buffer << std::endl;
+        /*클라이언트에게 문자 보내주기*/
+        send(client_sock, buffer, sizeof buffer, 0);
+        memset(buffer, '0', BUFSIZ); //버퍼지우기
+    }
+    closesocket(client_sock);
+    closesocket(server_sock);
+
+
+    // 맨 마지막에 넣어 줘야 함
+    WSACleanup(); // 원속 종료
+    return 0;
+}
+
